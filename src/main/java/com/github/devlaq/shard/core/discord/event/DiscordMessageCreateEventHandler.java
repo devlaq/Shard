dee8cc816f6abcc423465ef3a7fa39f86d7d5db0
devlaq <devlaq4918@gmail.com>
2022-01-20 20:08:09 +0900

    Fix: don't send messages in channel that not included to discord.channels.chatSyncChannels

--- src/main/java/com/github/devlaq/shard/core/discord/event/DiscordMessageCreateEventHandler.java
+++ src/main/java/com/github/devlaq/shard/core/discord/event/DiscordMessageCreateEventHandler.java
@@ -1,8 +1,17 @@
 package com.github.devlaq.shard.core.discord.event;
 
+import arc.util.Strings;
+import com.github.devlaq.shard.core.Global;
 import com.github.devlaq.shard.core.Me;
+import com.github.devlaq.shard.core.discord.Discord;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.Channel;
+import discord4j.core.object.entity.channel.MessageChannel;
 
 import java.util.function.Consumer;
 
@@ -11,9 +20,22 @@ public class DiscordMessageCreateEventHandler implements Consumer<MessageCreateE
     @Override
     public void accept(MessageCreateEvent event) {
         Message message = event.getMessage();
-        if(message.getAuthor().isPresent() && !message.getAuthor().get().isBot()) {
-            Me.discord(message.getAuthor().get().getTag(), message.getContent());
+        JsonObject discordConfiguration = Global.configuration.getAsJsonObject("discord");
+        if(discordConfiguration == null) return;
+        if(!discordConfiguration.get("enabled").getAsBoolean()) return;
+        JsonArray chatSyncChannels = discordConfiguration.getAsJsonObject("channels").getAsJsonArray("chatSyncChannels");
+        if(chatSyncChannels == null) return;
+        for(JsonElement jsonChannel : chatSyncChannels) {
+            String channelID = jsonChannel.getAsString();
+            Channel channel = Discord.gateway.getChannelById(Snowflake.of(channelID)).block();
+            if(!(channel instanceof MessageChannel messageChannel)) continue;
+            if(message.getChannel().block() == messageChannel) {
+                if(message.getAuthor().isPresent() && !message.getAuthor().get().isBot()) {
+                    Me.discord(message.getAuthor().get().getTag(), message.getContent());
+                }
+            }
         }
+
     }
 
 }
