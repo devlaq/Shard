789e1b43853e0b7d61f9dd61cf31faaaac63355a
devlaq <devlaq4918@gmail.com>
2022-04-01 00:27:48 +0900

    Update ShardCore.kt

--- core/src/main/kotlin/com/github/devlaq/shard/ShardCore.kt
+++ core/src/main/kotlin/com/github/devlaq/shard/ShardCore.kt
@@ -1,11 +1,12 @@
 package com.github.devlaq.shard
 
-abstract class ShardCore(val plugin: ShardPlugin): Module(null) {
+abstract class ShardCore : Module(null) {
 
     companion object {
         var instance: ShardCore? = null
     }
 
     var commands: Commands? = null
+    var events: EventHandlers? = null
 
 }
